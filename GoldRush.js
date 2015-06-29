caaa8a8e2cefc622af2c1ef6ad6cd69847a9da07
Lauren Shugrue <ellacorvo@gmail.com>
2015-06-29 15:34:40 -0600

    first commit

---
+++ GoldRush.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	$('body').on( "click", function(event) {
+  		var coordinatesX = event.pageX;
+  		var coordinatesY = event.pageY;
+
+  	 	console.log(
+    	    "pageX/Y: " +
+    	    event.pageX + ", " +
+    	    event.pageY
+        );
+
+  		$('body').append('<div class="container"><img class="marker" src="http://sr.photos3.fotosearch.com/bthumb/CSP/CSP666/k6660545.jpg"></div>');
+		
+		$('.container').last().css({ 'top' : coordinatesY , 'left' : coordinatesX });
+
+	});
+
+	$('body').on('click', '.container', function() {
+		$(this).remove();
+		// console.log('test');
+	});
+	
+	// $('.container').click(function() {
+	// 	$(this).remove();
+	// 	console.log('test');
+	// });
+
+  		
+});
