fdc8ea478ffd6e1fb5eef8173afcda6a3f8077c1
Lauren Shugrue <ellacorvo@gmail.com>
2015-06-30 16:29:42 -0600

    added content

--- main.js
+++ main.js
@@ -97,35 +97,52 @@
 
 //..........Loops Practice #3...........
 
-var students = [{
- name: 'Liz',
- age: 25,
- city: 'Boulder'
-},{
- name: 'Meghan',
- age: 27,
- city: 'Denver'
-},{
- name: 'Trent',
- age: 32,
- city: 'Boulder'
-},{
- name: 'Chelsea',
- age: 24,
- city: 'Boulder'
-},{
- name: 'Amir',
- age: 18,
- city: 'Denver'
-}];
+// var students = [{
+//  name: 'Liz',
+//  age: 25,
+//  city: 'Boulder'
+// },{
+//  name: 'Meghan',
+//  age: 27,
+//  city: 'Denver'
+// },{
+//  name: 'Trent',
+//  age: 32,
+//  city: 'Boulder'
+// },{
+//  name: 'Chelsea',
+//  age: 24,
+//  city: 'Boulder'
+// },{
+//  name: 'Amir',
+//  age: 18,
+//  city: 'Denver'
+// }];
 
 // for(var i=0; i<students.length; i++) {
 //  console.log(students[i].name);
 // }
 
-for(var i=0; i<students.length; i++) {
- console.log(students[i].age);
-}
+// for(var i=0; i<students.length; i++) {
+//  console.log(students[i].age);
+// }
+
+// for(var i=0; i<students.length; i++) {
+//  console.log(students[i].name + ", " + students[i].city);
+// }
+
+// for(var i=0; i<students.length; i++) {
+// 	if (students[i].city === 'Boulder') {
+//  	console.log(students[i].name + " is from " + students[i].city);
+//  	}
+// }
+
+// for(var i=0; i<students.length; i++) {
+// 	if (students[i].age > 25) {
+//  	console.log(students[i].name + " is older than 25");
+//  	}
+// }
+
 
 
 
